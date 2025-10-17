7cc20c68bb703f62b701b3939d238a327576861f
Daniel de Lima Franceschetti <0xfeedcafe@proton.me>
2025-10-17 15:03:54 -0300

    add user examples

--- main.sql
+++ main.sql
@@ -2,6 +2,46 @@ DROP DATABASE IF EXISTS sinasp;
 CREATE DATABASE sinasp;
 USE sinasp;
 
+-- Exemplos de usuários para o sistema:
+
+---- USUÁRIO ADMINISTRADOR GERAL
+--CREATE USER 'admin_sinasp'@'%' IDENTIFIED BY 'SenhaForte123!';
+--GRANT ALL PRIVILEGES ON sinasp.* TO 'admin_sinasp'@'%';
+--FLUSH PRIVILEGES;
+--
+---- USUÁRIOS DE LEITURA (para relatórios e dashboards)
+--CREATE USER 'reporting_user'@'%' IDENTIFIED BY 'Relatorio2025!';
+--GRANT SELECT ON sinasp.* TO 'reporting_user'@'%';
+--FLUSH PRIVILEGES;
+--
+---- USUÁRIO DE MÓDULO DE OCORRÊNCIAS
+--CREATE USER 'ocorrencia_user'@'%' IDENTIFIED BY 'Ocorrencia2025!';
+--GRANT SELECT, INSERT, UPDATE ON sinasp.ocorrencia TO 'ocorrencia_user'@'%';
+--GRANT SELECT ON sinasp.condutor, sinasp.pessoa, sinasp.veiculo TO 'ocorrencia_user'@'%';
+--FLUSH PRIVILEGES;
+--
+---- USUÁRIO DE MÓDULO DE INFRAÇÕES E MULTAS
+--CREATE USER 'infracao_user'@'%' IDENTIFIED BY 'Infracao2025!';
+--GRANT SELECT, INSERT, UPDATE ON sinasp.infracao, sinasp.multa, sinasp.recurso_infracao TO 'infracao_user'@'%';
+--GRANT SELECT ON sinasp.condutor, sinasp.veiculo, sinasp.pessoa TO 'infracao_user'@'%';
+--FLUSH PRIVILEGES;
+--
+---- USUÁRIO DE MÓDULO DE RECURSOS (veículos, armas, equipamentos)
+--CREATE USER 'recursos_user'@'%' IDENTIFIED BY 'Recursos2025!';
+--GRANT SELECT, INSERT, UPDATE ON sinasp.veiculo, sinasp.equipamento, sinasp.armamento TO 'recursos_user'@'%';
+--FLUSH PRIVILEGES;
+--
+---- USUÁRIO DE MÓDULO DE INVESTIGAÇÃO E PROCESSOS
+--CREATE USER 'investigacao_user'@'%' IDENTIFIED BY 'Investigacao2025!';
+--GRANT SELECT, INSERT, UPDATE ON sinasp.investigacao, sinasp.evidencia TO 'investigacao_user'@'%';
+--GRANT SELECT ON sinasp.ocorrencia, sinasp.pessoa, sinasp.usuario TO 'investigacao_user'@'%';
+--FLUSH PRIVILEGES;
+--
+---- USUÁRIO DE SUPORTE (apenas leitura para auditoria)
+--CREATE USER 'auditoria_user'@'%' IDENTIFIED BY 'Auditoria2025!';
+--GRANT SELECT ON sinasp.* TO 'auditoria_user'@'%';
+--FLUSH PRIVILEGES;
+
 -- A ordem de importação é crucial devido às dependências de chave estrangeira.
 
 -- 1. Tabelas de localização
